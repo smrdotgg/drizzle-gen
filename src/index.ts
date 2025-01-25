b46179c3138c3d73023a417432fbde3796a4800a
Semere Tereffe <se.semere.tereffe@gmail.com>
2025-01-25 03:06:14 +0300

    block on non-alpha

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,14 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Get package.json
+const pkg = await import("../package.json", { assert: { type: "json" } });
+
+if (!pkg.version.includes("alpha")) {
+  console.log("in development");
+  process.exit(0);
+}
+
 spawn("npx", ["tsx", join(__dirname, "..", "src", "main.ts")], {
   stdio: "inherit",
 });
