d362f811308b5b642ee16c2a1a0fa8f4de74d5e0
Semere Tereffe <se.semere.tereffe@gmail.com>
2025-01-25 03:07:12 +0300

    fix:block on non-alpha

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,22 @@ import { spawn } from "child_process";
 import { join } from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { readFileSync } from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Get package.json
-const pkg = await import("../package.json", { assert: { type: "json" } });
+const pkg = JSON.parse(
+  readFileSync(join(__dirname, "..", "package.json"), "utf-8")
+);
+
+
+if (!pkg.version.includes("alpha")) {
+
+  console.log("in development");
+  process.exit(0);
+}
 
 if (!pkg.version.includes("alpha")) {
   console.log("in development");
@@ -20,4 +30,3 @@ if (!pkg.version.includes("alpha")) {
 spawn("npx", ["tsx", join(__dirname, "..", "src", "main.ts")], {
   stdio: "inherit",
 });
-
